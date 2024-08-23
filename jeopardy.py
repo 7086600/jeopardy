3a9fda9490b31bc1748a55ca6d1861f6443ac181
Alexander <7086600@gmail.com>
2024-08-23 04:29:24 +0300

    lowercase words and questions directly in function filterDataQuestionsByWords; testing

--- jeopardy.py
+++ jeopardy.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import string
 
 pd.set_option('display.max_colwidth', 100)
 
@@ -16,29 +15,20 @@ df.rename(columns={
     ' Answer': 'answer'},
     inplace=True)
 
-# lowercase every question
-df['question'] = df.question.apply(str.lower)
-
 print(df.head())
 print()
 
 # step3: function that filters the dataset for questions that contains all of the words in a list of words
 def filterDataQuestionsByWords(data, words):
-    # lambda function return True if all of the words in the list of words are in a question 
-    checkQuestion = lambda question: all([True if word in question else False for word in words])
+    # lambda function return True if all of the words in the list of words are in a question
+    # lowercase both: word, question
+    checkQuestion = lambda question: all([True if word.lower() in question.lower() else False for word in words])
     # apply lambda function to question column and return data frame with True question column
     filterData = data[data.question.apply(checkQuestion)]
     return filterData
 
 # testing function
-words = ["King", "England"]
+words = ["KinG", "EnglanD"]
 fd = filterDataQuestionsByWords(df, words)
-print(fd.question)
-print()
-
-# step 4: lowercase words; testing function with various values
-words = ["King", "England"]
-wordsLower = [w.lower() for w in words]
-fd = filterDataQuestionsByWords(df, wordsLower)
 print(fd.question)
 print()
