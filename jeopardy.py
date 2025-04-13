907ffdf7d2e0a3242e8a3ba8b7f6ad5b5e776572
AK <67439739+7086600@users.noreply.github.com>
2025-04-13 17:55:22 +0300

    Go on project

--- jeopardy.py
+++ jeopardy.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-pd.set_option('display.max_colwidth', 70)
+pd.set_option('display.max_colwidth', 75)
 
 df = pd.read_csv('jeopardy.csv')
+# print(df.head(10))
 
 # step2: rename columns
 df.rename(columns={
@@ -15,10 +16,11 @@ df.rename(columns={
     ' Answer': 'answer'},
     inplace=True)
 
-print(df.iloc[39:56])
-print()
+# print(df.iloc[39:56])
+print(df.question.head(10))
 
 # step3: function that filters the dataset for questions that contains all of the words in a list of words
+
 def filterDataQuestionsByWords(data, words):
     # lambda function return True if all of the words in the list of words are in a question
     # lowercase both: word, question
@@ -28,17 +30,19 @@ def filterDataQuestionsByWords(data, words):
     return filterData
 
 # testing function
-words = ["viking"]
+words = ["King", "England"]
 fd = filterDataQuestionsByWords(df, words)
-# print(fd.head(5))
+# print(fd.head(10))
+# print(fd.info())
 print()
 
 # step 5: add column floatValue with converting values to float
 floatValue = lambda v: float(v.replace('$', '').replace(',', '').replace('no value', '0'))
 df['floatValue'] = df['value'].apply(floatValue)
-print(df.iloc[39:56])
+# print(df.iloc[100:121])
 print()
 # the average value of questions that contain the word
-word = ["KinG"]
+words = ["kinG"]
 fd = filterDataQuestionsByWords(df, words)
-print(f"The average value of questions that contain the word - {word[0].lower()} is {fd.floatValue.mean():.2f}")
+# print(fd.head(15))
+print(f"The average value of questions that contain the word - {words[0].lower()} is {fd.floatValue.mean():.2f}")
